ff06ce3bd418b04032ae8df568267b3069de3948
Dami Ogunmoye <dami@ogunmoye.com>
2020-05-16 02:35:50 +0100

    add        self.subscribe(PreferencesEvent, PreferencesLoadListener())

--- main.py
+++ main.py
@@ -6,55 +6,29 @@ from ulauncher.api.shared.action.RenderResultListAction import RenderResultListA
 from ulauncher.api.shared.action.OpenUrlAction import OpenUrlAction
 
 import logging
-import re
-import sys
 
 logger = logging.getLogger(__name__)
-shortcuts = {}
-
-def updateShortcuts(shortcutString):
-    shortcuts.clear
-    logger.info("Updating Shortcuts - %s" % (shortcutString))
-    shortcutPairs = shortcutString
-    for pair in shortcutPairs.split(';'):
-        keyValue = pair.split(':')
-        shortcuts[keyValue[0]] = keyValue[1]
-        logger.debug("Shortcut %s for %s" % (keyValue[0], keyValue[1]))
-
-def checkForShortcut(string):
-    if string in shortcuts:
-        logger.info("Shortcut found!  %s converted to %s" % (string, shortcuts[string]))
-        return shortcuts[string]
-    else:
-        return string
 
 class WherebyJoinMeeting(Extension):
     def __init__(self):
         super(WherebyJoinMeeting, self).__init__()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
-        self.subscribe(PreferencesEvent, PreferencesLoadListener())
-        self.subscribe(PreferencesUpdateEvent, PreferencesUpdateListener())
-
-class PreferencesLoadListener(EventListener):
-    def on_event(self, event, extension):
-        updateShortcuts(event.preferences['shortcuts'])
-
-class PreferencesUpdateListener(EventListener):
-    def on_event(self, event, extension):
-        if event.id == 'shortcuts':
-            updateShortcuts(event.new_value)
-
 
 class KeywordQueryEventListener(EventListener):
 
     def on_event(self, event, extension):
         baseUri = extension.preferences['base_uri']
 
+        defaultRoom = extension.preferences['default_room']
+
         logger.info("User Inputs: " + "--".join(event.get_argument()))
 
         chatId = event.get_argument()
 
-        fullUri = "https://" + baseUri + '/' + chatId
+        if len(chatId) == 0:
+            fullUri = "https://" + baseUri + '/' + defaultRoom
+        else:
+            fullUri = "https://" + baseUri + '/' + chatId
 
         resultItem = ExtensionResultItem(
             icon = 'images/whereby_icon.png',
